9782880b58193991a33bc0af2ce8f9bfa0489505
Sven Paaß <sven@paass.net>
2019-04-05 20:28:57 +0200

    assignment review

--- src/main/java/net/paass/spring5jokesapp/controllers/JokesController.java
+++ src/main/java/net/paass/spring5jokesapp/controllers/JokesController.java
@@ -1,6 +1,7 @@
 package net.paass.spring5jokesapp.controllers;
 
 import net.paass.spring5jokesapp.services.JokesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,14 +9,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class JokesController {
 
-  JokesService jokesService;
+  private JokesService jokesService;
 
+  @Autowired
   public JokesController(JokesService jokesService) {
     this.jokesService = jokesService;
   }
 
   @GetMapping({ "", "/" })
-  public String getRandomQuote(Model model) {
+  public String showJoke(Model model) {
     model.addAttribute("joke", jokesService.getJoke());
     return "chucknorris";
   }
--- src/main/java/net/paass/spring5jokesapp/services/JokesServiceImpl.java
+++ src/main/java/net/paass/spring5jokesapp/services/JokesServiceImpl.java
@@ -1,13 +1,12 @@
 package net.paass.spring5jokesapp.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JokesServiceImpl implements JokesService {
 
-  private ChuckNorrisQuotes chuckNorrisQuotes;
+  private final ChuckNorrisQuotes chuckNorrisQuotes;
 
   public JokesServiceImpl() {
     this.chuckNorrisQuotes = new ChuckNorrisQuotes();
