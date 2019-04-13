3c27a7f7b67b510faccd13c103e114295aae5f07
Sven Paaß <sven@paass.net>
2019-04-13 10:22:59 +0200

    xml-config-example

--- src/main/java/net/paass/spring5jokesapp/Spring5JokesAppApplication.java
+++ src/main/java/net/paass/spring5jokesapp/Spring5JokesAppApplication.java
@@ -1,13 +1,13 @@
 package net.paass.spring5jokesapp;
 
-import net.paass.spring5jokesapp.controllers.JokesController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "net.paass.spring5jokesapp", "guru.springframework"})
+@ImportResource("classpath:chuck-config.xml")
 public class Spring5JokesAppApplication {
 
   public static void main(String[] args) {
